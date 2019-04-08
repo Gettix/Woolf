4e4fc65162f0f6c6fee786c67340d8889393e899
Gettix <49359249+Gettix@users.noreply.github.com>
2019-04-09 00:08:55 +0700

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot,message,args) => {
     let enbed = new Discord.RichEmbed()
     .setDescription("Помощь")
     .setColor('#4B0082')
-    .addField("Модерация","kick,clear,warn,unwarn,ban,say,tempmute,addrole,report")
+    .addField("Модерация","kick(w!kick (пинг пользователя)) clear(w!clear (количество сообщений)) warn(w!warn (пинг пользователя)) unwarn(w!unwarn (пинг пользователя)) ban(w!ban (пинг пользователя)) say(w!say (ваше сообщение)) tempmute(w!tempmute (пинг пользователя) (сек./мин./час./дни)) report(w!report (пинг пользователя) (Причина или описание))")
     .addField("Информация","serverinfo,userinfo,help")
     .addField("Пригласить","https://discordapp.com/oauth2/authorize?client_id=551625781818818590&permissions=8&scope=bot")
     .setThumbnail(message.guild.iconURL)
