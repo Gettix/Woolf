9411aaf3c8e5ec790b927262a6068e109c89fd2c
Gettix <49359249+Gettix@users.noreply.github.com>
2019-04-09 07:58:50 +0700

    Create 8ball.js

---
+++ cmds/8ball.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js')
+const fs = require('fs')
+
+var fortunes = [
+  ":8ball: | Yas !",
+  ":8ball: | Hmmm let me think..",
+  ":8ball: | Maybe",
+  ":8ball: | possibly",
+  ":8ball: | Nope !",
+  ":8ball: | Never ~"
+];
